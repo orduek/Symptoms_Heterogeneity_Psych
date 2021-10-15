0c5191eb8a968acbb3d0d0d91af16b0d43b8b6bf
Tobias Spiller <54402019+TobiasRSpiller@users.noreply.github.com>
2021-10-15 11:03:40 +0200

    Added SD for bootstrap & changed one-sided results

--- Analysis/PHQ9/SCRIPT_II_PHQ9_V2.r
+++ Analysis/PHQ9/SCRIPT_II_PHQ9_V2.r
@@ -134,6 +134,10 @@ m_pl$pars # 2.43
 bs_p = bootstrap_p(m_pl, no_of_sims = 1000, threads = 5, seed = 241)
 bs_p$p
 
+# SD 
+sd(bs_p$bootstraps$xmin)
+sd(bs_p$bootstraps$pars)
+
 pdf("PL_parameters_boot_PHQ9.pdf", width=8, height=8)
 plot(bs_p)
 dev.off() 
@@ -154,10 +158,9 @@ lines(m_ln_EQ, col = 4,lty = 2, lwd = 2)
 dev.off()
 
 # Formally assess
-compare_distributions(m_pl, m_ln_EQ)$p_two_sided # p < 0.748 -> one of the two has better fit
-compare_distributions(m_pl, m_ln_EQ)$p_one_sided #   p < 0.62 -> m_ln_EQ  better fit
-compare_distributions(m_ln_EQ, m_pl)$p_one_sided #   p < 0.37 -> m_pl better fit
-
+compare_distributions(m_pl, m_ln_EQ)$p_two_sided # p < 0.05 -> one of the two has better fit
+compare_distributions(m_pl, m_ln_EQ)$p_one_sided #   p < 0.05 -> m_pl  better fit
+compare_distributions(m_ln_EQ, m_pl)$p_one_sided #   p < 0.05 -> m_ln_EQ better fit
 
 ######  6. Export data for Figures ##############################################
 ### Figure 1a
